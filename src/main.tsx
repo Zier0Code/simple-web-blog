f9701c90898987bd3456e1110354e6bfd2e5e384
Angelo <momoxsana01@gmail.com>
2025-06-05 14:29:56 +0800

    added some text

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import { router } from "./routes/routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <>
+      <h2 className="font-bold p-4 text-center">
+        Welcome to Web Blog - ReactJS, TailwindCSS, Supabase +{" "}
+      </h2>
+      <RouterProvider router={router} />
+    </>
   </StrictMode>
 );
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Handle registration logic here
+    console.log("Registration form submitted");
+  };
+
   return (
     <>
-      <div>Register</div>
+      <div className="flex justify-center">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4">
+          <h1 className="font-bold text-xl pb-2">Register</h1>
+          <p>
+            Already have an Account ?{" "}
+            <Link className="text-blue-600 hover:underline" to={"/login"}>
+              {" "}
+              Login.
+            </Link>
+          </p>
+          <div className="max-w-md w-auto mt-5 flex flex-col gap-2">
+            <input
+              className="p-2 border-2 border-gray-300 rounded-2xl"
+              placeholder="Email"
+              type="email"
+            />
+            <input
+              className="p-2 border-2 border-gray-300 rounded-2xl"
+              placeholder="Password"
+              type="password"
+            />
+          </div>
+          <button
+            className="bg-black text-white p-2 rounded-2xl hover:bg-gray-800 transition-colors duration-300"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 };
